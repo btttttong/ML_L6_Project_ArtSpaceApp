fdb267b4b7a29c42c0f0e284ee7eb59c65ef9b46
supakavadee r <tong.7147@gmail.com>
2024-11-23 19:58:18 +0700

    update code layout

--- app/src/main/java/com/supakavadeer/m5_l6_project_artspaceapp/MainActivity.kt
+++ app/src/main/java/com/supakavadeer/m5_l6_project_artspaceapp/MainActivity.kt
@@ -11,24 +11,24 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
-import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
@@ -56,9 +56,8 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun ArtSpaceApp(modifier: Modifier = Modifier) {
     val context = LocalContext.current
-    val currentIndex = remember { mutableStateOf(0) }
-    val imageIds: List<Int> = R.drawable::class.java.fields
-        .filter { it.name.contains("cat") }
+    val currentIndex = remember { mutableIntStateOf(0) }
+    val imageIds: List<Int> = R.drawable::class.java.fields.filter { it.name.contains("cat") }
         .map { field -> field.getInt(null) }
 
     val artworks = imageIds.mapNotNull { imageId ->
@@ -78,23 +77,18 @@ fun ArtSpaceApp(modifier: Modifier = Modifier) {
     }
 
     if (artworks.isNotEmpty()) {
-        val currentArtwork = artworks[currentIndex.value]
-        ArtSpaceLayout(
-            modifier,
-            artWork = currentArtwork,
-            onPreviousClick =
-            {
-                if (currentIndex.value > 0) {
-                    currentIndex.value -= 1
-                }
-            },
-            onNextClick =
-            {
-                if (currentIndex.value < artworks.size - 1) {
-                    currentIndex.value += 1
-                }
+        val currentArtwork = artworks[currentIndex.intValue]
+        ArtSpaceLayout(modifier, artWork = currentArtwork, onPreviousClick = {
+            if (currentIndex.intValue > 0) {
+                currentIndex.value -= 1
             }
-        )
+        }, onNextClick = {
+            if (currentIndex.intValue < artworks.size - 1) {
+                currentIndex.value += 1
+            } else {
+                currentIndex.intValue = 0
+            }
+        })
     } else {
         Text(
             text = "No images found!"
@@ -109,80 +103,73 @@ fun ArtSpaceLayout(
     onPreviousClick: () -> Unit,
     onNextClick: () -> Unit
 ) {
-    Scaffold(
-        modifier = modifier.fillMaxSize(),
-        bottomBar = {
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(40.dp),
-                horizontalArrangement = Arrangement.SpaceBetween
+    Scaffold(modifier = modifier.fillMaxSize(), bottomBar = {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(24.dp),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Button(
+                onClick = onPreviousClick, modifier = Modifier.size(width = 120.dp, height = 40.dp)
             ) {
-                Button(
-                    onClick = onPreviousClick,
-                    modifier = Modifier.size(width = 120.dp, height = 40.dp)
-                ) {
-                    Text(text = "Previous")
-                }
-                Button(
-                    onClick = onNextClick,
-                    modifier = Modifier.size(width = 120.dp, height = 40.dp)
-                ) {
-                    Text(text = "Next")
-                }
+                Text(text = "Previous")
+            }
+            Button(
+                onClick = onNextClick, modifier = Modifier.size(width = 120.dp, height = 40.dp)
+            ) {
+                Text(text = "Next")
             }
         }
-    ) { innerPadding ->
+    }) { innerPadding ->
         Column(
             modifier = Modifier
                 .fillMaxSize()
                 .padding(innerPadding),
             horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
+            verticalArrangement = Arrangement.SpaceEvenly
+
         ) {
             Box(
                 modifier = Modifier
-                    .padding(16.dp)
-                    .size(400.dp)
-                    .border(20.dp, Color.White, shape = RectangleShape)
-                    .shadow(
-                        elevation = 2.dp
-                    )
+                    .fillMaxHeight(0.6f)
+                    .aspectRatio(3f / 4f)
+                    .border(4.dp, Color.White, shape = RectangleShape)
+                    .shadow(elevation = 2.dp)
+                    .padding(8.dp)
             ) {
                 Image(
                     painter = painterResource(id = artWork.imageId),
                     contentDescription = artWork.title,
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .padding(8.dp),
+                    modifier = Modifier.fillMaxSize(),
                     contentScale = ContentScale.Crop
                 )
             }
 
             Column(
                 modifier = Modifier
-                    .padding(top = 100.dp)
-                    .size(250.dp, 100.dp)
-                    .background(Color(0xFFecebf4)),
-                verticalArrangement = Arrangement.Center
+                    .fillMaxWidth(0.6f)
+                    .background(Color(0xFFecebf4), shape = MaterialTheme.shapes.medium)
+                    .padding(16.dp),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.Start
 
-                ) {
+            ) {
                 Text(
                     text = artWork.title,
                     style = MaterialTheme.typography.headlineSmall,
-                    modifier = Modifier.padding(bottom = 4.dp, start = 4.dp)
+                    modifier = Modifier.padding(bottom = 8.dp)
                 )
                 Text(
                     text = "${artWork.artist} (${artWork.year})",
                     style = MaterialTheme.typography.bodyMedium,
-                    modifier = Modifier.padding(start = 4.dp)
                 )
             }
         }
     }
 }
 
-@Preview(showBackground = true, showSystemUi = true)
+@Preview(showBackground = true, showSystemUi = true, device = "id:pixel_9")
 @Composable
 fun ArtSpacePreview() {
     M5_L6_Project_ArtSpaceAppTheme {
